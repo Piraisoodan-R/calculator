378c7134f8f41c1de474eeaee0d79a15b6826392
piraisoodan-Dev <piraisoodanrajasekaran@gmail.com>
2025-12-31 16:38:18 +0530

    first commit

---
+++ calc-script.js
@@ -0,0 +1,16 @@
+function dispvalue(val)
+{
+ document.getElementById("display").value=document.getElementById("display").value+val
+}
+function dispclear()
+{
+ document.getElementById("display").value =" ";
+}
+function calculate()
+{
+     var userinput = document.getElementById("display").value;
+     var result = eval(userinput);
+
+     
+     document.getElementById("display").value = result;
+}
